62631772b5be036e67846488cc748e9b3cf85da7
glorial83 <glorial@naver.com>
2024-09-30 13:01:17 +0900

    'sqss'

---
+++ sqss.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         sqss
+// @namespace    http://tampermonkey.net/
+// @version      2024-09-30
+// @description  try to take over the world!
+// @author       You
+// @include      https://sqss.kr/lessons/2%eb%93%b1%ea%b8%89_*
+// @icon         https://sqss.kr/wp-content/uploads/2023/06/cropped-logo-32x32.png
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // 감시할 타겟 노드를 선택합니다.
+  const targetNode = document.body;
+  //alert('player' + (player));
+
+  player.debug = true;
+  player.preset["fast-forward"] = true;
+  player.preset.progress = true;
+  player.preset.speed = true;
+  player.preset.pip = true;
+})();
