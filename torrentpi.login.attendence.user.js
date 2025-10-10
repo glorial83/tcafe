97cfde9ae93978b85cb21b3d9923b13d4ddab28d
glorial83 <glorial@naver.com>
2025-10-10 13:27:04 +0900

    도메인 변경

--- torrentpi.login.attendence.user.js
+++ torrentpi.login.attendence.user.js
@@ -4,7 +4,7 @@
 // @version      2025-10-10
 // @description  try to take over the world!
 // @author       glorial
-// @match        https://torrentpi*.com/bbs/board.php?bo_table=chulsuk
+// @match        https://torrentpi160.com/bbs/board.php?bo_table=chulsuk
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torrentpi160.com
 // @grant        none
 // ==/UserScript==
