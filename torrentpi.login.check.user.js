21e84c39a97902457b50d2a3b7dfcee7f0d990d9
glorial83 <glorial@naver.com>
2025-10-10 13:15:07 +0900

    도메인 변경

--- torrentpi.login.check.user.js
+++ torrentpi.login.check.user.js
@@ -4,8 +4,8 @@
 // @version      2025-04-21
 // @description  try to take over the world!
 // @author       glorial
-// @match        https://*.torrentpi150.com/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=torrentpi150.com
+// @match        https://*.torrentpi*.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=torrentpi160.com
 // @grant        none
 // ==/UserScript==
 
