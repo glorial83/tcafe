98d7657b82d64561d7c2ecf597ae6ed9364b3c9d
glorial83 <glorial@naver.com>
2026-04-28 09:19:48 +0900

    추천 50이상인 것들에게 하이라이트

---
+++ torrentpi.pds.upper50.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         torrentpi.pds.upper50
+// @namespace    http://tampermonkey.net/
+// @version      2026-04-28
+// @description  try to take over the world!
+// @author       glorial
+// @include      https://*.torrentpi*.com/bbs/board.php?bo_table=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=torrentpi160.com
+// @grant        none
+// ==/UserScript==
+// 추천이 50이상인 것들만 하이라이트 처리
+
+(function() {
+    'use strict';
+    const items = document.querySelectorAll('#fboardlist > div.list-board > ul > li > div:nth-child(7)');
+
+    items.forEach(div => {
+        const value = parseInt(div.innerText.trim());
+        if (value >= 50) {
+            // 여기에 적용하고 싶은 스타일을 작성하세요
+            div.parentElement.style.backgroundColor='#e3f2fd';
+            div.parentElement.style.fontWeight = 'bold';
+        }
+    });
+
+    const pdsTable = document.querySelector("#thema_wrapper > div.at-body > div > div > div.col-md-9.pull-right.at-col.at-main > div.row.at-row > div > div.col-sm-9");
+    pdsTable.style.width = '100%';
+})();
